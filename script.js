6bacf39e5b4b0148f567187df35469f90a183dec
Himanshu9425 <147365994+Himanshu9425@users.noreply.github.com>
2025-02-28 13:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timerdisplay=document.querySelector("#timer");
+
+
+
+setInterval(
+	function(){
+		let currentDate=new Date();
+		timerdisplay.innerHTML=currentDate.toLocaleString();
+	},1000
+)
